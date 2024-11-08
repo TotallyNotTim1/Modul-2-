db9e3eadf05d289c8499bb2c43c9721d1f4f7dff
TotallyNotTim1 <timmielolhype@gmail.com>
2024-11-08 17:17:59 +0100

    Add files via upload

---
+++ Uppgift 2/script/script.js
@@ -0,0 +1,21 @@
+let vip = true
+let age = 17
+
+var event = alert
+
+
+
+    if(vip) {
+        alert("Welcome in my best vip customer :D")
+    } else
+    if(age>= 18) {
+      alert("Access granted");
+    } else {
+    alert ("Access denied");
+    }
+
+
+
+
+
+
